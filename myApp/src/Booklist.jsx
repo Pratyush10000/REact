60a9a7f0313270a01fcaf2f809bea581ece9d89d
Pratyush10000 <800pratyush@gmail.com>
2024-04-30 14:37:40 +0530

    sample

--- myApp/src/Booklist.jsx
+++ myApp/src/Booklist.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import Book from './Book'
+import React from 'react';
+import Book from './Book';
 
-const data1={
-  title: "The Catcher in the Rye",
-  author:"J.D. Salinger",
-  imgLink:"https://m.media-amazon.com/images/I/7108sdEUEGL._AC_UY327_FMwebp_QL65_.jpg",
-  price:"₹352"
-}
+const BookData = [
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    imgLink: "https://m.media-amazon.com/images/I/7108sdEUEGL._AC_UY327_FMwebp_QL65_.jpg",
+    price: "₹352",
+  },
+  {
+    title: "THE RICHEST MAN IN BABYLON",
+    author: "George S. Clason ",
+    imgLink: "https://m.media-amazon.com/images/I/61TMqBUodWL._AC_UY327_FMwebp_QL65_.jpg",
+    price: "₹400",
+  },
+  {
+    title: "Wabi Sabi : The Wisdom in Imperfection",
+    author: "Nobuo Suzuki",
+    imgLink: "https://m.media-amazon.com/images/I/611Y5rMSogL._AC_UY327_FMwebp_QL65_.jpg",
+    price: "₹250",
+  },
+  {
+    title: "Think Straight: Change your thoughts ",
+    author: " Darius Foroux",
+    imgLink: "https://m.media-amazon.com/images/I/71XpkRq0ncL._AC_UY327_FMwebp_QL65_.jpg",
+    price: "₹200",
+  },
+  {
+    title: "Believe in Yourself",
+    author: "Dr Joseph Murphy",
+    imgLink: "https://m.media-amazon.com/images/I/51JoXgnIciL._AC_UY327_FMwebp_QL65_.jpg",
+    price: "₹500",
+  },
+];
 
 const Booklist = () => {
   return (
     <div className="booklist">
-      <Book 
-      title={data1.title} author={data1.author}  imgLink={data1.imgLink} price={data1.price} />
+      {BookData.map((book, index) => (
+        <Book
+          key={index}
+          title={book.title}
+          author={book.author}
+          imgLink={book.imgLink}
+          price={book.price}
+        />
+      ))}
     </div>
   );
 };
